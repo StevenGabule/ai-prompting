adc71fb29ab9e3508a88da9aa25f8efd4e21f657
John Paul Gabule <lucas.gabule@gmail.com>
2025-01-10 06:29:07 +0800

    add readme

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8001", // Points to your Next.js API route
+  baseURL: "http://localhost:8001", 
   headers: {
     "Content-Type": "application/json",
   },
